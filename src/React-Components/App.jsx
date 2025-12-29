d66e52a6aa85673187566face1c8e120d1559735
pedamallu-vamsi <vamsitkd@gmail.com>
2025-12-29 15:31:20 +0530

    Added Admin component in App.jsx

--- src/React-Components/App.jsx
+++ src/React-Components/App.jsx
@@ -2,6 +2,7 @@ import Home from "./Home"
 import Why from "./Why"
 import Waiter from "./Waiter"
 import KitchenSection from "./Kitchen";
+import AdminDashboardSection from "./Admin"
 function App(){
     return(
         <>
@@ -9,6 +10,7 @@ function App(){
         <Why/>
         <Waiter/>
         <KitchenSection/>
+        <AdminDashboardSection/>
         </>
     );
 }
