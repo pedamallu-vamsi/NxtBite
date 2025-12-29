5d2b71ce605cd4e97a44f9c0ed3245d00d3d3ed0
pedamallu-vamsi <vamsitkd@gmail.com>
2025-12-29 10:00:04 +0530

    Completed React Component for Home page

---
+++ React-Components/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="Home">
+      <h1 className="Home-Heading">NxtBite</h1>
+      <p className="Home-Para">
+        From QR to Customer Loyalty — A New Era of Dining Experience
+      </p>
+    </div>
+  );
+}
+
+export default Home;
