2867b8c9fb7e6d91dad5b25fcf905391beca836d
pedamallu-vamsi <vamsitkd@gmail.com>
2025-12-29 15:57:32 +0530

    Added Client component to app.jsx

--- src/React-Components/App.jsx
+++ src/React-Components/App.jsx
@@ -3,11 +3,13 @@ import Why from "./Why"
 import Waiter from "./Waiter"
 import KitchenSection from "./Kitchen";
 import AdminDashboardSection from "./Admin"
+import ClientSection from "./Client"
 function App(){
     return(
         <>
         <Home/>
         <Why/>
+        <ClientSection/>
         <Waiter/>
         <KitchenSection/>
         <AdminDashboardSection/>
